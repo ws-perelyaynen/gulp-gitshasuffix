4c25711f68b73c24c759efaa075d97e21aac6d79
John Doe <kronos.ariec@gmail.com>
2017-12-10 22:00:13 +0300

    manifest added

--- README.md
+++ README.md
@@ -15,8 +15,9 @@ this uses the sha hash from the latest git commit.
 First, install `gulp-gitshasuffix` as a development dependency:
 
 ```shell
-npm install --save-dev gulp-gitshasuffix
+ npm i --save-dev https://github.com/ws-perelyaynen/gulp-gitshasuffix.git
 ```
+<!-- npm install --save-dev gulp-gitshasuffix -->
 
 Then, add it to your `gulpfile.js`:
 
@@ -51,19 +52,43 @@ gulp.task('moveFiles'. function () {
 ### gitshasuffix(options)
 
 #### options.length
-Type: `Integer`  
+Type: `Integer`
 Default: 6
 
 Length of the sha to show.
 
 #### options.separator
-Type: `String`  
+Type: `String`
 Default: "-"
 
 Separator before the suffix.
 
+#### options.manifest
+Type: `String`
+Default: "./manifest.json"
+
+creates manifest file with all renames
+Useful for templating (for example Jade or Pug)
+
+```
+{"path/to/file.ext" : "path/to/file-hash.ext"}
+```
+
+#### options.trimRelatedPath
+Type: `String`
+Default: ''
+
+trim related path, useful when Jade template placed in subfolder
+**options.manifest** is required
+Useful for templating (for example Jade or Pug)
+
+```
+{"path/to/file.ext" : "path/to/file-hash.ext"}
+```
+
+
 #### options.folder
-Type: `Boolean`  
+Type: `Boolean`
 Default: "false"
 
 If the sha-substring should be as a folder instead of suffix.
@@ -111,7 +136,7 @@ New path:  /Users/example/gulp-gitshasuffix/test/fixtures/c-eaa51c.txt
 0.1.0:
  * Changes to using options object instead of two parameters on input.
 
-0.0.4: 
+0.0.4:
  * No longer throws unjust error on streamed contents.
 
 ## License
--- index.js
+++ index.js
@@ -1,62 +1,108 @@
-var through = require("through2"),
-  gutil = require("gulp-util"),
-  path = require("path"),
-  git = require("./lib/git");
+var through = require('through2'),
+	gutil = require('gulp-util'),
+	path = require('path'),
+	fs = require('fs'),
+	git = require('./lib/git');
 
 module.exports = function (options) {
-  "use strict";
+	'use strict';
 
-  var sha = null;
+	var sha = null;
 
-  options = options || {};
-  var length = options.length || 6;
-  var separator = options.separator || options.seperator || "-";
-  var folder = !!options.folder;
+	options = options || {};
+	var length = options.length || 6;
+	var separator = options.separator || options.seperator || '-';
+	var trimRelatedPath  = options.trimRelatedPath || '';
+	var manifest = '';
+	var manifestData = {};
+	if (options.manifest) {
+		manifest = process.cwd() + '/' + options.manifest;
+	} else {
+		manifest = process.cwd() + '/./manifesssst.json';
+	}
+	var folder = !!options.folder;
 
-  var gitshasuffix = function (file, enc, callback) {
-    var stream = this,
-        dir = path.dirname(file.path),
-        ext = path.extname(file.relative),
-        firstname = path.basename(file.relative, ext),
-        finalname = null;
+	var gitshasuffix = function (file, enc, callback) {
+		var stream = this,
+			dir = path.dirname(file.path),
+			ext = path.extname(file.relative),
+			firstname = path.basename(file.relative, ext),
+			finalname = null;
 
-    var appendSuffix = function () {
-      var shaSuffix = sha.substring(0, length),
-          finalSuffix = separator + shaSuffix;
+		var appendSuffix = function () {
+			var shaSuffix = sha.substring(0, length),
+				finalSuffix = separator + shaSuffix;
 
-      if (file.isNull()) {
-        file.path = file.path + finalSuffix;
-        stream.push(file);
-        return callback();
-      }
+			if (file.isNull()) {
+				file.path += finalSuffix;
+				stream.push(file);
+				return callback();
+			}
 
-      if (folder) {
-        dir = path.join(dir, shaSuffix);
-        finalSuffix = '';
-      }
+			if (folder) {
+				dir = path.join(dir, shaSuffix);
+				finalSuffix = '';
+			}
 
-      file.revOrigPath = file.path;
-      file.revOrigBase = file.base;
-      file.path = path.join(dir, firstname + finalSuffix + ext);
-      stream.push(file);
-      return callback();
-    };
+			file.revOrigPath = file.path;
+			file.revOrigBase = file.base;
+			var relativePathKey = path.relative(process.cwd(), file.path);
+			file.path = path.join(dir, firstname + finalSuffix + ext);
+			var relativePathValue = path.relative(process.cwd(), file.path);
+			if (trimRelatedPath) {
+				relativePathKey =	path.relative(trimRelatedPath, relativePathKey);
+				relativePathValue =	path.relative(trimRelatedPath, relativePathValue);
+			}
+			manifestData[ relativePathKey ] = relativePathValue;
+			stream.push(file);
+			return callback();
+		};
 
-    if (!!sha) {
-      return appendSuffix();
-    }
-    git.getLatestSha(function (err, latestSha) {
-      if (err) {
-        stream.emit('error', new gutil.PluginError('gulp-gitshasuffix', err));
-        return callback();
-      }
-      sha = latestSha;
-      appendSuffix();
-    });
-  }
+		if (sha) {
+			return appendSuffix();
+		}
+		git.getLatestSha(function (err, latestSha) {
+			if (err) {
+				stream.emit('error', new gutil.PluginError('gulp-gitshasuffix', err));
+				return callback();
+			}
+			sha = latestSha;
+			appendSuffix();
+		});
+	};
 
-  return through.obj(gitshasuffix, function (callback) {
-    sha = null;
-    return callback();
-  });
+	function saveManifestFile (manifestPath, data, callback) {
+		if (manifestPath) {
+			try {
+				fs.statSync(manifestPath);
+			} catch (error) {
+				fs.writeFileSync(manifestPath, '');
+			}
+			var readedJson;
+			try {
+				readedJson = fs.readFileSync(manifestPath, 'utf8');
+			} catch (error) {
+			}
+			if (readedJson) {
+				readedJson = JSON.parse(readedJson);
+				data = JSON.parse(data);
+				Object.assign(readedJson, data);
+				readedJson = JSON.stringify(readedJson);
+				return fs.writeFileSync(manifestPath, readedJson, callback);
+			} else {
+				return fs.writeFileSync(manifestPath, data, callback);
+			}
+		} else {
+			callback();
+		}
+	}
+
+	return through.obj(gitshasuffix, function (callback) {
+		sha = null;
+		if (options.manifest) {
+			return saveManifestFile(manifest, JSON.stringify(manifestData), callback);
+		} else {
+			return callback();
+		}
+	});
 };
